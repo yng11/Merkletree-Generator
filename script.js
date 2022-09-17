4b3cdd6dd088eae16e9e425ec3b4a5a308e598e1
yng11 <106720553+yng11@users.noreply.github.com>
2022-09-17 18:28:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function clickButton(){
+    let str = document.querySelector("textarea").value.replace(/ /g,"");
+    let addrList = str.split(",");
+
+    const leaves = addrList.map(x => keccak256(x));
+    const tree = new MerkleTree(leaves, keccak256, {sortPairs: true});
+    const buf2hex = x => '0x' + x.toString('hex');
+
+
+    document.querySelector("p").innerHTML = "머클루트 : " + buf2hex(tree.getRoot());
+}
