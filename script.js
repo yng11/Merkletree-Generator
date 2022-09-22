2d11ce9a3b24712f9b4f0ce909475fc4fc848a9e
yng11 <106720553+yng11@users.noreply.github.com>
2022-09-22 10:44:53 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function clickButton(){
-    let str = document.querySelector("textarea").value.replace(/ /g,"");
-    str = str.replace(/\n/g, "");
-    let addrList = str.split(",");
+function clickButton() {
+  let str = document.querySelector("textarea").value.replace(/ /g, "");
+  str = str.replace(/\n/g, "");
+  str = str.replace(/\"/gi, "");
+  str = str.replace(/,\s*$/, "");
+  let addrList = str.split(",");
 
-    const leaves = addrList.map(x => keccak256(x));
-    const tree = new MerkleTree(leaves, keccak256, {sortPairs: true});
-    const buf2hex = x => '0x' + x.toString('hex');
+  const leaves = addrList.map((x) => keccak256(x));
+  const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+  const buf2hex = (x) => "0x" + x.toString("hex");
 
-
-    document.querySelector("p").innerHTML = "머클루트 : " + buf2hex(tree.getRoot());
+  document.querySelector("p").innerHTML =
+    "머클루트 : " + buf2hex(tree.getRoot());
 }
